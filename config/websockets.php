27671527317f711eeef94c5652bd75315bca6805
Sarvar <sarvar_web@texnomart.uz>
2022-10-21 17:54:00 +0500

    test

--- config/websockets.php
+++ config/websockets.php
@@ -114,13 +114,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', "/etc/letsencrypt/live/test.lipe.uz/fullchain.pem"),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', "/etc/letsencrypt/live/test.lipe.uz/fullchain1.pem"),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', "/etc/letsencrypt/live/test.lipe.uz/privkey.pem"),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', "/etc/letsencrypt/live/test.lipe.uz/privkey1.pem"),
 
         /*
          * Passphrase for your local_cert file.
